63906ce07700164b61baba88c478f72ceaa6d5d0
2pasha <kostishinpavlo24@gmail.com>
2024-08-24 15:15:52 +0300

    make build

---
+++ dist/index.32ad9972.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});var e=document.getElementById("formId");e.addEventListener("submit",function(t){t.preventDefault(),alert("Дякуємо за довіру)"),e.reset()});
+//# sourceMappingURL=index.32ad9972.js.map
---
+++ dist/index.73ea5ea1.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.73ea5ea1.js.map
