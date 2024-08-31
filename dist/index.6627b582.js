5b0d3e6826d48e1fa95e948cde46bd2eb0e077c3
2pasha <kostishinpavlo24@gmail.com>
2024-08-31 12:52:50 +0300

    make build

---
+++ dist/index.6627b582.js
@@ -0,0 +1,2 @@
+var n=document.getElementById("toTopButton");window.addEventListener("scroll",function(){window.scrollY>470?n.style.display="block":n.style.display="none"});
+//# sourceMappingURL=index.6627b582.js.map
